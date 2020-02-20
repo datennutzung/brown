0c6ef14a7ee3287d4ffe38fbce4cc62ccfc192b8
Miron Gertis <56720084+datennutzung@users.noreply.github.com>
2020-02-20 23:46:50 +0100

    brown

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var body = document.body;
 var black_square = document.getElementById("black-square");
+var elem = document.documentElement;
 
 reset();
 
 function hide_black_square() {
-    window.addEventListener('click', function() {fade_to_white()}, once=true)
+    window.addEventListener('click', function() {fade_to_white()}, once=true);
     black_square.hidden = true;
 }
 
 function fade_to_white() {
-    window.addEventListener('click', function() {reset()}, once=true)
     body.classList.add('white-background');
+    setTimeout(function() {
+        window.addEventListener('click', function() {reset()}, once=true);
+    }, 7000)
+    
 }
 
 function reset() {
     black_square.hidden = false;
     body.classList.remove('white-background');
-    window.addEventListener('click', function() {hide_black_square()}, once=true)
+    window.addEventListener('click', function() {hide_black_square()}, once=true);
 }
